5c9a18afbf51c32f2dae22a78a7fdef008d02d94
Alexandre Kandalintsev <exe.sre@gmail.com>
2021-05-19 14:09:54 +0200

    main.py: refactor usb-serial code into a class

--- main.py
+++ main.py
@@ -11,49 +11,50 @@ from datetime import datetime
 import click
 import serial
 
-ser = serial.Serial(
-    port="/dev/ttyUSB0",
-    baudrate=19200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    xonxoff=1,
-    timeout=5,
-)
 
+class K2000:
+    def __init__(self, keep_display_off=True):
+        self.ser = serial.Serial(
+            port="/dev/ttyUSB0",
+            baudrate=19200,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            xonxoff=1,
+            timeout=5,
+        )
+        self.init(keep_display_off)
 
-def query(query, echo=True, response=True, to_float=False):
-    if echo:
-        print(f"query: {query}")
-    raw_query = bytes(query + "\r\n", "utf8")
-    ser.write(raw_query)
-    # time.sleep(0.5)
-    if response:
-        raw_resp = ser.readline().strip()
-        resp = raw_resp.decode()
+    def query(self, query, echo=True, response=True, to_float=False):
         if echo:
-            print(f"response: {resp}")
-        if to_float:
-            resp = float(resp)
-        return resp
+            print(f"query: {query}")
+        raw_query = bytes(query + "\r\n", "utf8")
+        self.ser.write(raw_query)
+        # time.sleep(0.5)
+        if response:
+            raw_resp = self.ser.readline().strip()
+            resp = raw_resp.decode()
+            if echo:
+                print(f"response: {resp}")
+            if to_float:
+                resp = float(resp)
+            return resp
 
+    def init(self, keep_display_off=True):
+        data = self.query("*IDN?\r\n")
+        assert "KEITHLEY" in data, "IDN failed"
+        self.query(":CONF:VOLT:DC", response=False)
+        self.query(":SENS:VOLT:NPLC 10", response=False)
+        self.query(":CONF?")
+        self.query(":DISP:ENAB 0", response=False)
+        if not keep_display_off:
+            atexit.register(lambda: self.query(":DISP:ENAB 1", response=False))
 
-def init(keep_display_off=True):
-    data = query("*IDN?\r\n")
-    assert "KEITHLEY" in data, "IDN failed"
-    query(":CONF:VOLT:DC", response=False)
-    query(":SENS:VOLT:NPLC 10", response=False)
-    query(":CONF?")
-    query(":DISP:ENAB 0", response=False)
-    if not keep_display_off:
-        atexit.register(lambda: query(":DISP:ENAB 1", response=False))
-
-
-def read_data(echo=False):
-    data = query(":READ?", to_float=True)
-    if echo:
-        print(f"{data:.6f} {datetime.now()}")
-    return data
+    def read_data(self, echo=False):
+        data = self.query(":READ?", to_float=True)
+        if echo:
+            print(f"{data:.6f} {datetime.now()}")
+        return data
 
 
 def wait_til_next_tick(step=1, extra=-0.0005):
@@ -76,14 +77,14 @@ def main(output, period, keep_display_off):
         print(f'output file "{output}" already exists!', file=sys.stderr)
         raise click.Abort()
 
-    init()
+    k2000 = K2000(keep_display_off)
 
     with open(output, "wt", newline="\n") as f:
         writer = csv.writer(f)
         while True:
             wait_til_next_tick(period)
             ts = datetime.now()
-            data = read_data()
+            data = k2000.read_data()
             writer.writerow([ts, data])
             f.flush()
             print(f"{data:2.6f}   {ts}")
